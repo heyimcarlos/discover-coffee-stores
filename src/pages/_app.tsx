a8bf1c5f6a09d9fd65a8a1f52e4e2b7758713dd2
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2022-04-12 20:13:13 -0400

    Finishing section one of Complete Next.js developer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppPropsWithLayout } from 'next/app';
+// components
 import { Fragment } from 'react';
-import '../styles/globals.css';
+// styles
+import '@styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // if Component.getLayout is different than null | undefined. run.
   // const getLayout = Component.getLayout ?? (page => page);
   const Layout = Component.layout ?? Fragment;
+
   return (
     <div>
       {/* {getLayout(<Component {...pageProps} />)} */}
