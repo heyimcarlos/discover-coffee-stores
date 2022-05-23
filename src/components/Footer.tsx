ad0a299522d1030a5ced75376ae380a179cb103f
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2022-05-23 00:15:24 -0400

    Adding some sugar to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 // styles
 import styles from '@style/home.module.css';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <a href='https://github.com/renzik' target='_blank' rel='noopener noreferrer'>
-        Powered by <h3 className={styles.footerLogo}>Renzik</h3>
-      </a>
+      <small>© 2022 All Rights Reserved | Coffee Connoisseur</small>
+      <Link href='https://github.com/renzik' target='_blank' rel='noopener noreferrer'>
+        <a>
+          <div className={styles.footerContent}>
+            <span>Powered by</span>
+            <span>&nbsp;</span>
+            <h3 className={styles.footerLogo}>Renzik</h3>
+          </div>
+        </a>
+      </Link>
     </footer>
   );
 };
